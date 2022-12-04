c9456d8c4935d2131ead3363b618a12df137ff41
Thaw De Zin <thawdezin@gmail.com>
2022-12-04 22:03:02 +0630

    Initial Commit

---
+++ TwoTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoTV
+//
+//  Created by Thaw De Zin on 12/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
